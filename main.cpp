ceb0a8b25b3059b0b16a22961644cafdc1fa7b7a
pouminii <pouminii@naver.com>
2025-05-11 02:12:50 +0900

    Changed the project name

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 int main()
 {
-	Core::Window::Instance().Initialize(2000, 1000, "Vulkan");
+	Core::Window::Instance().Initialize(2000, 1000, "Anggeum Engine");
 
 	ApplicationOptions options{ false, &Core::Window::Instance() };
 	Application application(options);
